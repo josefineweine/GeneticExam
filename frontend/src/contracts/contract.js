6437233c3d767e7614226ec4c5081f877fb4da1c
josefineweine <114985858+josefineweine@users.noreply.github.com>
2025-02-20 06:30:15 +0100

    Update contract.js

--- frontend/src/contracts/contract.js
+++ frontend/src/contracts/contract.js
@@ -1,5 +1,6 @@
-// Load contract ABI
-const contractABI = require('./contract-abi.json');  
 
-export const CONTRACT_ABI = contractABI;
-export const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS; 
+
+export const CONTRACT_ABI = require('./contract-abi.json');  
+export const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+
+console.log("Contract Address:", CONTRACT_ADDRESS);
